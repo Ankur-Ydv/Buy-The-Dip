1e80b75d56e26ad4bf01567e66f6bf148c86055e
Ankur Yadav <ankury.2508@gmail.com>
2022-12-15 21:07:48 +0530

    Added Info Page

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import Coin from './Coin';
-import logo from './logo.png';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Info from "./pages/Info";
 
-function App() {
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
-      )
-      .then(res => {
-        setCoins(res.data);
-        console.log(res.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
-
-  const filteredCoins = coins.filter(coin =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
-
+const App = () => {
   return (
-    <div className='coin-app'>
-      <div className='coin-search'>
-      <img src={logo} alt='logo'></img>
-        <form>
-          <input
-            className='coin-input'
-            type='text'
-            onChange={handleChange}
-            placeholder='Search'
-          />
-        </form>
-      </div>
-      {filteredCoins.map(coin => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.total_volume}
-            volume={coin.market_cap}
-            image={coin.image}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        );
-      })}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:coinId" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
